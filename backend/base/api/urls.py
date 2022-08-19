e3a8b5ae0123d419d08ab846ca5c933ca0e0040d
Zaeem Akhtar <zmaktr@gmail.com>
2022-08-19 11:49:55 +0500

    override jwt views to claim username for decode payload inside jwt.io

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from . import views
+from . views import MyTokenObtainPairView
 
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
+    #TokenObtainPairView, # decode token claims
     TokenRefreshView,
 )
 
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -2,7 +2,24 @@ import imp
 from django.http import JsonResponse
 from rest_framework.response import Response 
 from rest_framework.decorators import api_view
+# customizing token claims for decode
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
+# customizing token claims for decode
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+# get endpoints
 @api_view(['GET'])
 # this view/function is created to present all our endpoints
 def getRoutes(request):
