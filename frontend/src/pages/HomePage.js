20b2aabb7076652c1d9d43a7e8f72cf86324596f
unknown <zmaktr@gmail.com>
2022-09-10 21:31:59 +0500

    api endpoint changed to subdomain

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,42 +1,42 @@
-import React, { useState, useEffect, useContext } from 'react';
-import AuthContext from '../context/AuthContext';
+import React, { useState, useEffect, useContext } from "react";
+import AuthContext from "../context/AuthContext";
 
 const HomePage = () => {
-  
-  let [notes, setNotes] = useState([])
-  
-  useEffect(()=> {
-    getNotes()
-  }, [])
-  
-  let {authTokens, logoutUser} = useContext(AuthContext)
-  
+  let [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  let { authTokens, logoutUser } = useContext(AuthContext);
+
   let getNotes = async () => {
-    let response = await fetch('http://localhost:8000/api/notes/', {
-      method:'GET',
+    let response = await fetch("https://django-react-auth-backend/api/notes/", {
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization':'Bearer ' + String(authTokens.access)
-      }
-    })
-    let data = await response.json()
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + String(authTokens.access),
+      },
+    });
+    let data = await response.json();
 
     if (response.status === 200) {
-      setNotes(data)
+      setNotes(data);
+    } else if (response.statusText === "Unauthorized") {
+      logoutUser();
     }
-    else if(response.statusText === 'Unauthorized') {
-      logoutUser()
-    }
-  }
+  };
   return (
     <div>
-        <p>You are inside the homePage</p>
-        <p>Only Logged in users can access this</p>
-        <ul>
-          {notes.map(note => ( <li key={note.id}>{note.body}</li> ))}
-        </ul>
+      <p>You are inside the homePage</p>
+      <p>Only Logged in users can access this</p>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.body}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
