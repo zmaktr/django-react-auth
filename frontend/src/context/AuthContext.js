7b98d1ec68c67d319ed5d03f7f7b36ceb9d66f41
unknown <zmaktr@gmail.com>
2022-09-10 21:28:28 +0500

    errors removed before deployment

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,100 +1,115 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState, useEffect } from "react";
 import jwt_decode from "jwt-decode";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 // Create a context
-
-const AuthContext = createContext()
-
+const AuthContext = createContext();
 export default AuthContext;
 
-// Provider
+// Create a provider
+export const AuthProvider = ({ children }) => {
+  let [authTokens, setAuthTokens] = useState(() =>
+    localStorage.getItem("authTokens")
+      ? JSON.parse(localStorage.getItem("authTokens"))
+      : null
+  );
 
-export const AuthProvider = ({children}) => {
-    
-    let [authTokens, setAuthTokens] = useState( ()=> localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
-    let [user, setUser] = useState( ()=> localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null)
-    let [loading, setLoading] = useState(false)
-    
-    const history = useNavigate()
+  let [user, setUser] = useState(() =>
+    localStorage.getItem("authTokens")
+      ? jwt_decode(localStorage.getItem("authTokens"))
+      : null
+  );
 
-    let loginUser = async (e) => {
-        e.preventDefault()
-        let response = await fetch('http://127.0.0.1:8000/api/token/', {
-            method :'POST',
-            headers : {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({'username':e.target.username.value, 'password':e.target.password.value})
-        })
-        let data = await response.json()
+  let [loading, setLoading] = useState(false);
 
-        if (response.status === 200) {
-                setAuthTokens(data)
-                setUser(jwt_decode(data.access))
-                localStorage.setItem('authTokens', JSON.stringify(data))
-                history('/');
-                console.log('sucessAuthContent promise resolve')
-        }
-        else {
-            alert('Something went wong | incorrect credentials')
-        }
+  const history = useNavigate();
+
+  let loginUser = async (e) => {
+    e.preventDefault();
+    let response = await fetch(
+      "https://django-react-auth-backend.zaeemakhtar.site/api/token/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: e.target.username.value,
+          password: e.target.password.value,
+        }),
+      }
+    );
+    let data = await response.json();
+
+    if (response.status === 200) {
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access));
+      localStorage.setItem("authTokens", JSON.stringify(data));
+      history("/");
+      console.log("sucessAuthContent promise resolve");
+    } else {
+      alert("Something went wong | incorrect credentials");
     }
+  };
 
-    let logoutUser = () => {
-        setAuthTokens(null)
-        setUser(null)
-        localStorage.removeItem('authTokens')
-        history('/login');
+  let logoutUser = () => {
+    setAuthTokens(null);
+    setUser(null);
+    localStorage.removeItem("authTokens");
+    history("/login");
+  };
+
+  let updateToken = async () => {
+    console.log("update token called");
+    let response = await fetch(
+      "https://django-react-auth-backend.zaeemakhtar.site/api/token/refresh/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ refresh: authTokens?.refresh }),
+      }
+    );
+    let data = await response.json();
+
+    if (response.status === 200) {
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access));
+      localStorage.setItem("authTokens", JSON.stringify(data));
+    } else {
+      logoutUser();
     }
-
-    let updateToken = async () => {
-        console.log('update token called')
-        let response = await fetch('http://127.0.0.1:8000/api/token/refresh/', {
-            method :'POST',
-            headers : {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({'refresh':authTokens?.refresh})
-        })
-        let data = await response.json()
-
-        if (response.status === 200) {
-            setAuthTokens(data)
-            setUser(jwt_decode(data.access))
-            localStorage.setItem('authTokens', JSON.stringify(data))
-        }
-        else{
-            logoutUser()
-        }
-        if (loading) {
-            setLoading(false)
-        }
+    if (loading) {
+      setLoading(false);
     }
+  };
 
-    let contextData = {
-        user:user,
-        authTokens:authTokens,
-        loginUser:loginUser,
-        logoutUser:logoutUser,
+  let contextData = {
+    user: user,
+    authTokens: authTokens,
+    loginUser: loginUser,
+    logoutUser: logoutUser,
+  };
+
+  useEffect(() => {
+    if (loading) {
+      updateToken();
     }
+    let fourandHalfMinutes = 1000 * 60 * 4.5;
+    let interval = setInterval(() => {
+      if (authTokens) {
+        updateToken();
+      }
+    }, fourandHalfMinutes);
 
-    useEffect(()=> {
-        if (loading) {
-        updateToken()
-        }
-        let fourandHalfMinutes = 1000 * 60 * 4.5
-        let interval =setInterval(()=> {
-            if(authTokens){
-                updateToken()
-            }
-        }, fourandHalfMinutes)
-        return ()=> clearInterval(interval)
-    }, [authTokens, loading])
+    return () => clearInterval(interval);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [authTokens, loading]);
 
-    return(
-        <AuthContext.Provider value={contextData}>
-            {loading ? null : children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider value={contextData}>
+      {loading ? null : children}
+    </AuthContext.Provider>
+  );
+};
