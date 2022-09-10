c9769be5786f2e1d764ea198d01ef699fa580649
unknown <zmaktr@gmail.com>
2022-09-11 00:03:03 +0500

    fill in login form with values

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,18 +1,29 @@
-import React, { useContext } from 'react';
-import AuthContext from '../context/AuthContext';
+import React, { useContext } from "react";
+import AuthContext from "../context/AuthContext";
 
 const LoginPage = () => {
-  let {loginUser} = useContext(AuthContext)
+  let { loginUser } = useContext(AuthContext);
   return (
     <div>
       <p>Please fill in to login</p>
-        <form onSubmit={loginUser}>
-            <input type='text' name='username' placeholder='Enter Username'/>
-            <input type='password' name='password' placeholder='Enter Password' autoComplete='on'/>
-            <input type='submit'/>
-        </form>
+      <form onSubmit={loginUser}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Enter Username"
+          value="admin"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          autoComplete="on"
+          value="admin"
+        />
+        <input type="submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
